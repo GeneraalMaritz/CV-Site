0dc07fab56f46b02e29549deb2da23127b7a0add
GeneraalMaritz <janusmaritz7@gmail.com>
2021-12-07 19:44:29 +0200

    Commit

--- mywebsite/mywebsite/settings.py
+++ mywebsite/mywebsite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-yymw#stig^eyvbvq=0p2niya9hs^akujve0_u+n=p9ino04e4b
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'janusmaritz-cv.herokuapp.com']
 
 
 # Application definition
@@ -120,9 +120,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/staticfiles/'
-#STATICFILES_DIRS = [os.path.join(BASE_DIR, "staticfiles")]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "staticfiles")]
 MEDIA_URL = '/images/'
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
